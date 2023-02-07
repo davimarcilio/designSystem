b75370559fe7553ab2a229aee32511dcbf575ab8
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-07 16:14:07 -0300

    Form

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
     children: 'enviar',
