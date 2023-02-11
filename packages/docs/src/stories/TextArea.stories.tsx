1cda7fe738f5dd1a614a6a7439a8c26387e90395
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-11 10:44:14 -0300

    TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,32 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextArea',
+  component: TextArea,
+
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as={'label'}
+          css={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+        >
+          <Text size={'sm'}>Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'type your text here',
+  },
+}
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
