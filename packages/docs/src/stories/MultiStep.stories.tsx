53273b954d599919d7a612e5ef7968d6fd790702
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-11 21:39:06 -0300

    MultiStep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,32 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as={'label'}
+          css={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {
+  args: {},
+}
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
