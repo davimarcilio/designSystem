ccc4ac85f88bbb4b45f89fc2d1064882abe1c638
Davi Marcilio <davimarcilio.js@gmail.com>
2023-02-12 11:53:13 -0300

    workflow permission

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/designSystem/'
+    }
+
+    return config
   }
 }
